5217bed0f4e971139f647ab00e61e676ef99cb44
vincepzhang <vincepzhang@tencent.com>
2024-04-26 17:07:33 +0800

    --bugfix： Fix screen sharing failure

--- TRTC-Simple-Demo/ios/Flutter/flutter_export_environment.sh
+++ TRTC-Simple-Demo/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tatemin/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tatemin/Desktop/workspace/source/project/github/TRTC_Flutter/TRTC-Simple-Demo"
+export "FLUTTER_ROOT=/Users/vincepzhang/package/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vincepzhang/UGit/work_sapce/liteav_flutter/sdk/trtc/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
